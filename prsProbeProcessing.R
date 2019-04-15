3ccb2523977b43f2516d0f3ce7dc2bedfaddd3ec
Stevan Earl <stevan.earl@asu.edu>
2019-04-15 11:58:19 -0700

    winter 2018-2019
    in addition to adding the winter 2018-2019 data, i also rearranged the processing script to reverse chronological

--- prsProbeProcessing.R
+++ prsProbeProcessing.R
@@ -19,45 +19,23 @@ pg <- pg_prod
 pg <- pg_local
 
 
-# FULL-SUITE DATA ----
+# SESSION: winter 2018-2019 -----------------------------------------------
 
-# SESSION: 2015-2016 winter deployment ----
+winter_2018_2019  <- read_excel('~/Desktop/Nutrient Supply Rate Data_ Project 1895_ Sally Wittlinger.xlsx',
+                                skip = 5)
 
-# read_csv removed the A \ B from the
-# Sample.ID column...weird read_excel could not handle the dates
-data <- read.csv('~/Desktop/Nutrient Supply Rate Data_ Project 1572_ Sally Wittlinger.csv', skip = 5, stringsAsFactors = F, na.strings = "NA")
+newprs <- winter_2018_2019 %>% 
+  filter(!is.na(`Sample ID`)) %>% 
+  rename(`Total-N` = `Total N`) %>% 
+  gather(id, result, `Total-N`:`NH4-N`) %>% # stack
+  mutate(
+    plotid = as.numeric(gsub("[[:alpha:]]", "", `Sample ID`)),
+    location = ifelse(gsub("[[:digit:]]", "", `Sample ID`, ignore.case = T) == 'A', 'under plant',
+                      ifelse(gsub("[[:digit:]]", "", `Sample ID`, ignore.case = T) == 'B', 'between plant', NA)), # location
+    location = ifelse(plotid > 75, 'BLANK', location), # location if blank
+    flag = ifelse(result <= 2.0, "below detection limit", NA) # flag bdl
+  ) 
 
-# SESSION: 2016 summer deployment (full-suite) ----
-
-# did not have any problems with read_excel but used read.csv so I could use
-# the code below with minimal reworking. Had to change the date format and name
-# of cations and anions but otherwise everything below worked fine
-data <- read.csv('~/Desktop/Nutrient Supply Rate Data_ Project 1673_ Sally Wittlinger.csv', skip = 5, stringsAsFactors = F, na.strings = "NA")
-
-moddat <- data %>%
-  filter(!is.na(Sample.ID)) %>%
-  filter(Sample.ID != "") %>%
-  gather(id, result, Total.N:Cd) %>%
-  mutate(burial_date = as.POSIXct(Burial.Date, format = "%Y-%m-%d")) %>%
-  mutate(retrieval_date = as.POSIXct(Retrieval.Date, format = "%Y-%m-%d")) %>%
-  mutate(plotid = as.numeric(str_match(Sample.ID, "\\d+"))) %>%
-  mutate(location = ifelse(gsub("[[:digit:]]", "", Sample.ID, ignore.case = T) == 'A', 'under plant',
-                    ifelse(gsub("[[:digit:]]", "", Sample.ID, ignore.case = T) == 'B', 'between plant', NA))) %>%
-  mutate(location = ifelse(plotid > 75, 'BLANK', location)) %>%
-  mutate(flag = ifelse(result <= 2.0 & (id == 'Total.N' | id == 'NO3..N' | id == 'NH4..N' | id == 'Ca' | id == 'S'), "below detection limit", NA)) %>%
-  mutate(flag = ifelse(result <= 4.0 & ( id == 'Mg' | id == 'K' ), "below detection limit", flag)) %>%
-  mutate(flag = ifelse(result <= 0.2 & ( id == 'P' | id == 'Mn' | id == 'Cu' | id == 'Zn' | id == 'B' | id == 'Pb' | id == 'Cd'), "below detection limit", flag)) %>%
-  mutate(flag = ifelse(result <= 0.4 & ( id == 'Fe' | id == 'Al' ), "below detection limit", flag)) %>%
-  mutate(id = gsub("\\.", "-", id)) %>%
-  mutate(id = gsub("\\--", "-", id)) %>%
-  mutate(WAL.. = as.numeric(WAL..)) %>%
-  mutate(Notes = ifelse(Notes == "", NA, Notes)) %>%
-  select(WAL.., plotid, burial_date, retrieval_date, id, result, flag, location, X.Cation = X..Cation, X.Anion = X..Anion, Notes)
-
-moddat <- moddat %>% mutate(result = as.numeric(result)) # had to add this for summer 2016
-
-
-# N-ONLY DATA ----
 
 # SESSION: summer 2018 ----
 
@@ -95,7 +73,6 @@ newprs <- winter_2017_2018 %>%
   ) 
 
 
-
 # SESSION: summer 2017 ----
 
 # using read.csv so as to be copasetic with the prsmod function
@@ -130,12 +107,50 @@ winter_2016_2017 <- winter_2016_2017 %>%
   mutate(id = gsub("\\.", "-", id)) 
 
 
+# SESSION: 2016 summer (full-suite) ----
+
+# did not have any problems with read_excel but used read.csv so I could use
+# the code below with minimal reworking. Had to change the date format and name
+# of cations and anions but otherwise everything below worked fine
+data <- read.csv('~/Desktop/Nutrient Supply Rate Data_ Project 1673_ Sally Wittlinger.csv', skip = 5, stringsAsFactors = F, na.strings = "NA")
+
+moddat <- data %>%
+  filter(!is.na(Sample.ID)) %>%
+  filter(Sample.ID != "") %>%
+  gather(id, result, Total.N:Cd) %>%
+  mutate(burial_date = as.POSIXct(Burial.Date, format = "%Y-%m-%d")) %>%
+  mutate(retrieval_date = as.POSIXct(Retrieval.Date, format = "%Y-%m-%d")) %>%
+  mutate(plotid = as.numeric(str_match(Sample.ID, "\\d+"))) %>%
+  mutate(location = ifelse(gsub("[[:digit:]]", "", Sample.ID, ignore.case = T) == 'A', 'under plant',
+                           ifelse(gsub("[[:digit:]]", "", Sample.ID, ignore.case = T) == 'B', 'between plant', NA))) %>%
+  mutate(location = ifelse(plotid > 75, 'BLANK', location)) %>%
+  mutate(flag = ifelse(result <= 2.0 & (id == 'Total.N' | id == 'NO3..N' | id == 'NH4..N' | id == 'Ca' | id == 'S'), "below detection limit", NA)) %>%
+  mutate(flag = ifelse(result <= 4.0 & ( id == 'Mg' | id == 'K' ), "below detection limit", flag)) %>%
+  mutate(flag = ifelse(result <= 0.2 & ( id == 'P' | id == 'Mn' | id == 'Cu' | id == 'Zn' | id == 'B' | id == 'Pb' | id == 'Cd'), "below detection limit", flag)) %>%
+  mutate(flag = ifelse(result <= 0.4 & ( id == 'Fe' | id == 'Al' ), "below detection limit", flag)) %>%
+  mutate(id = gsub("\\.", "-", id)) %>%
+  mutate(id = gsub("\\--", "-", id)) %>%
+  mutate(WAL.. = as.numeric(WAL..)) %>%
+  mutate(Notes = ifelse(Notes == "", NA, Notes)) %>%
+  select(WAL.., plotid, burial_date, retrieval_date, id, result, flag, location, X.Cation = X..Cation, X.Anion = X..Anion, Notes)
+
+moddat <- moddat %>% mutate(result = as.numeric(result)) # had to add this for summer 2016
+
+
+# SESSION: 2015-2016 winter ----
+
+# read_csv removed the A \ B from the
+# Sample.ID column...weird read_excel could not handle the dates
+data <- read.csv('~/Desktop/Nutrient Supply Rate Data_ Project 1572_ Sally Wittlinger.csv', skip = 5, stringsAsFactors = F, na.strings = "NA")
+
+
 # generic processing function ----
 
 # WesternAg is not consistent with their output, may need to adjust this
 # function slightly to accomodate different date formats and column names note
 # May 2016: in fact unuseable for winter 2015-2016 deployment owing, at least in
 # part, to full suite of ions
+
 prsmod <- function(dataframe) {
 
   dataMod <- dataframe %>%
