509470ba58da5332456f303582d73e25fc0e92a9
Stevan Earl <stevan.earl@asu.edu>
2022-06-02 16:55:47 -0700

    feat: add another date validation; add ctags

--- helper_validate_deployment.R
+++ helper_validate_deployment.R
@@ -1,21 +1,30 @@
 #' @title check for reasonable (< 180 d) deployment period
 #'
 #' @description `validate_deployment_times` checks to ensure that reported
-#' deployment dates are reasonable (less than 180 days in this case). The
-#' purpose of this validation is to help identify deployment dates that may
+#' deployment dates are reasonable (less than 180 days; greater than 0 days).
+#' The purpose of this validation is to help identify deployment dates that may
 #' have been entered incorrectly.
 #'
 #' @export
 
 validate_deployment_times <- function(prs_data) {
 
-prs_data |>
-  pointblank::col_vals_lt(
-    columns       = vars(date_range),
-    value         = 180,
-    na_pass       = TRUE,
-    preconditions = function(x) { dplyr::mutate(x, date_range = lubridate::ymd(x$`Retrieval Date`) - lubridate::ymd(x$`Burial Date`))},
-    actions       = pointblank::stop_on_fail()
-  )
+  prs_data |>
+    pointblank::col_vals_lt(
+      columns       = vars(date_range),
+      value         = 180,
+      na_pass       = TRUE,
+      preconditions = function(x) { dplyr::mutate(x, date_range = lubridate::ymd(x$`Retrieval Date`) - lubridate::ymd(x$`Burial Date`))},
+      actions       = pointblank::stop_on_fail()
+    )
+
+  prs_data |>
+    pointblank::col_vals_gt(
+      columns       = vars(date_range),
+      value         = 0,
+      na_pass       = TRUE,
+      preconditions = function(x) { dplyr::mutate(x, date_range = lubridate::ymd(x$`Retrieval Date`) - lubridate::ymd(x$`Burial Date`))},
+      actions       = pointblank::stop_on_fail()
+    )
 
 }
